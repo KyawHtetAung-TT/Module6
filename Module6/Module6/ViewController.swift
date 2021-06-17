239b7e018935ab2de635c66090c787304f237a5e
Ryan Willson <93kyawhtetaung@gmail.com>
2021-06-17 22:24:12 +0630

    01 init arrays

---
+++ Module6/Module6/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Module6
+//
+//  Created by Ryan Willson on 6/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
+
+// Creating an Array
+// String, Int, Double, Float, Object, Enum, Bool ...
+
+let sampleStirng : [String] = []
+let sampleStirng2 : Array<String> = Array()
+let sampleString3 : [String] = ["value1","value2"]
+let sampleStirng4 = ["value1","value2"]
+let sampleStirng5 : Any = ["value","value2",true]
+
+// inspecting an array isEmpty, count
+
+sampleStirng.isEmpty
+
